31fc169a5a02e2596306078776b4d04852a3b1b0
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-09-26 21:40:36 +0700

    fix respon for update avatar

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -14,10 +14,12 @@ class UserService {
     return await userModel.findOne({ email }).select(select).lean();
   };
 
+  static findById = async id => userModel.findOne({ _id: id}).lean();
+
   static updateAvatarService = async (file, keyStore) => {
     if (!file) throw new BadRequestError("file not found");
 
-    const user = await userModel.findOne({ _id: keyStore.user }).lean();
+    const user = await this.findById(keyStore.user)
     if (!user) throw new BadRequestError("user not found");
 
     const rs = await userModel.updateOne(
@@ -28,8 +30,10 @@ class UserService {
     // check update success
     if (!rs.acknowledged) throw new BadRequestError("update avatar error");
 
+    const userResult = await this.findById(keyStore.user)
+
     return {
-      user: getInfoData({ fileds: ["_id", "name", "avatar"], object: rs }),
+      user: getInfoData({ fileds: ["_id", "name", "avatar"], object: userResult }),
     };
   };
 
