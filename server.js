90548fc1bfcf4dc818131a26649f40dabf303c02
Nguyễn Ngọc Đức <106262560+ngocduc63@users.noreply.github.com>
2024-11-03 20:15:19 +0700

    fix repeat call api

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const app = require("./src/app")
+const axios = require("axios");
 const  PORT = 3055 
 
 const  server = app.listen(PORT, '0.0.0.0' , ()=>{
    console.log(`API SOCIAL NETWORK start with  ${PORT}` )
 })
-
-app.get("/", (req, res) => res.send("Deploy sucesss"));
 
 // process.on("SIGINT",()=>{
 //     server.close(()=> console.log(`Exit server express`))
@@ -13,13 +12,13 @@ app.get("/", (req, res) => res.send("Deploy sucesss"));
 // })
 
 const callApiEveryMinute = () => {
-   axios.get(`https://0.0.0.0:${PORT}/`)
-       .then(response => {
-           console.log("API called successfully:", response.data);
+   axios.get(`https://0.0.0.0:${PORT}/api/user/image/avt_default_male`)
+       .then(() => {
+           console.log("API called successfully");
        })
        .catch(error => {
            console.error("Error calling API:", error.message);
        });
 };
 
-setInterval(callApiEveryMinute, 60000);
+setInterval(callApiEveryMinute, 100);
